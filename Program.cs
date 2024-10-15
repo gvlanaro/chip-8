44ea88dec78c8ea9556ef6685da0944176cae46c
Giovanni Lanaro <lanaro@tutamail.com>
2024-10-15 11:05:02 +0200

    removed roms

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     static void Main(string[] args)
     {
-        using (Window window = new Window(640, 320, "chip-8", "roms/test5.ch8"))
+        using (Window window = new Window(640, 320, "chip-8", "/home/glanaro/Games/roms/Chip8_roms/test5.ch8"))
         {
             window.UpdateFrequency = 60;
             window.Run();
